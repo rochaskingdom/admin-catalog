e2d5a87b2fe2bf6ae2d399d25b4f4a5a0f157ca9
Vinicius Rocha <viniciusr.rocha@outlook.com>
2023-01-07 20:49:27 -0300

    feat: Adicionado mockito

--- application/src/test/java/com/vinicius/admin/catalog/application/UseCaseTest.java
+++ application/src/test/java/com/vinicius/admin/catalog/application/UseCaseTest.java
@@ -1,2 +1,8 @@
-package com.vinicius.admin.catalog.application;public class UseCaseTest {
+package com.vinicius.admin.catalog.application;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class UseCaseTest {
 }
