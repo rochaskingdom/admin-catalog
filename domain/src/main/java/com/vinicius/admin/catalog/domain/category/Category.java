738cc566eafd6a8284c845a77195878f730caf7e
Vinicius Rocha <viniciusr.rocha@outlook.com>
2023-01-07 18:26:31 -0300

    feat: Tornado a classe Category em um AggregateRoot

--- domain/src/main/java/com/vinicius/admin/catalog/domain/category/Category.java
+++ domain/src/main/java/com/vinicius/admin/catalog/domain/category/Category.java
@@ -1,11 +1,11 @@
 package com.vinicius.admin.catalog.domain.category;
 
+import com.vinicius.admin.catalog.domain.AggregateRoot;
+
 import java.time.Instant;
-import java.util.UUID;
 
-public class Category {
+public class Category extends AggregateRoot<CategoryID> {
 
-    private String id;
     private String name;
     private String description;
     private boolean active;
@@ -14,30 +14,30 @@ public class Category {
     private Instant deletedAt;
 
     private Category(
-            final String id,
+            final CategoryID id,
             final String name,
             final String description,
-            final boolean active,
-            final Instant createdAt,
-            final Instant updatedAt,
-            final Instant deleteddAt
+            final boolean isActive,
+            final Instant creationDate,
+            final Instant updateDate,
+            final Instant deleteDate
     ) {
-        this.id = id;
+        super(id);
         this.name = name;
         this.description = description;
-        this.active = active;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.deletedAt = deleteddAt;
+        this.active = isActive;
+        this.createdAt = creationDate;
+        this.updatedAt = updateDate;
+        this.deletedAt = deleteDate;
     }
 
     public static Category newCategory(final String aName, final String aDescription, final boolean isActive) {
-        final var id = UUID.randomUUID().toString();
+        final var id = CategoryID.unique();
         final var now = Instant.now();
         return new Category(id, aName, aDescription, isActive, now, now, null);
     }
 
-    public String getId() {
+    public CategoryID getId() {
         return id;
     }
 
